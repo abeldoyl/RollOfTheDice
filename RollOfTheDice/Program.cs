0f86b8489fd32c34d2642ce7524ac91cfa2f570a
abeldoyl <abeldoyle71@gmail.com>
2026-02-22 10:30:29 -0700

    add two dice rolls and a separator for the rows

--- RollOfTheDice/Program.cs
+++ RollOfTheDice/Program.cs
@@ -9,10 +9,16 @@
 
             for (int i = 0; i < 1000; i++)
             {
-                int total = rand.Next(1, 13);
-                counts[total]++;
+                int die1 = rand.Next(1, 7); // dice #1 for rolls 1–6
+                int die2 = rand.Next(1, 7); // dice #2 for rolls 1–6
+                int sum = die1 + die2; //sum of both die for a 2-12 result
+
+                counts[sum]++;
             }
-            Console.WriteLine("Roll Of The Dice".PadLeft(150));
+            Console.WriteLine("Roll Of The Dice".PadLeft(165));
+            Console.WriteLine("  2  |   3  |   4  |   5  |   6  |   7  " +
+                "|   8  |   9  |  10  |  11  |  12  "); // column headers
+            Console.WriteLine((new string('-', 75))); // row separator
 
             //pause
             Console.Read();
