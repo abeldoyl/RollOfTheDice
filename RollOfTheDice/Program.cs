b97ac763e235386091bea4cc8e9a7d3b4ed23cb4
abeldoyl <abeldoyle71@gmail.com>
2026-02-22 10:17:24 -0700

    Add project files.

---
+++ RollOfTheDice/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RollOfTheDice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = new int[13];
+            Random rand = new Random();
+            string message = "Roll Of The Dice";
+            message += message.ToString().PadLeft(150);
+
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int total = rand.Next(1, 13);
+                counts[total]++;
+            }
+            Console.Write(message);
+
+            //pause
+            Console.Read();
+        }
+    }
+}
